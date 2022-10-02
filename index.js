7d6be394f43647d0c2881d7e7acb4ed6f06217c3
MF249 <mfaizel@knights.ucf.edu>
2022-10-02 16:43:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const apiRoutes = require('./routes/api.js');
 var mongoUtil = require('./mongoUtil');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json()); 
 app.use('/api', apiRoutes);
 
 mongoUtil.connectToServer( function( err, client ) {
   if (err) console.log(err);
-  app.listen(PORT, () => console.log('Server running on port 5000...'));
+  app.listen(PORT, () => console.log('Server running on port...'));
 });
