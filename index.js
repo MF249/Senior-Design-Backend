6b05c61e17c81be40777aec4a10ceed63aab3911
MF249 <mfaizel@knights.ucf.edu>
2022-10-02 18:58:48 -0400

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api.js');
+const cors = require('cors');
 var mongoUtil = require('./mongoUtil');
 
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json()); 
 app.use('/api', apiRoutes);
+app.use(cors());
 
 mongoUtil.connectToServer( function( err, client ) {
   if (err) console.log(err);
