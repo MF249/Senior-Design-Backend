1e5fdbd2c1828a784e5de720bb9dee44e70e1993
MF249 <mfaizel@knights.ucf.edu>
2022-10-05 11:20:28 -0400

    update cors implementation

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 const apiRoutes = require('./routes/api.js');
-const cors = require('cors');
 var mongoUtil = require('./mongoUtil');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json()); 
 app.use('/api', apiRoutes);
-app.use(cors());
 
 app.use((req, res, next) => 
 {
@@ -26,6 +24,6 @@ app.use((req, res, next) =>
 });
 
 mongoUtil.connectToServer( function( err, client ) {
-  if (err) console.log(err);
-  app.listen(PORT, () => console.log('Server running on port...'));
+    if (err) console.log(err);
+    app.listen(PORT, () => console.log('Server running on port...'));
 });
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt  = require('jsonwebtoken');
 const mongoUtil = require('../mongoUtil');
 const sgMail = require('@sendgrid/mail');
 const User = require('../models/newUser.js');
 const ObjectID = require('mongodb').ObjectID;
 
+router.use(cors());
 require('dotenv').config();
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-const router = express.Router();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.get('/getAll', async (req, res) => {
         
@@ -23,24 +26,41 @@ router.post('/login', async (req, res) => {
     username = req.body.username;
     password = req.body.password;
 
-    console.log('Searching for ' + username + " :");
-
     let db_connect = mongoUtil.getDb("AppTest");
-    let myquery = { username: username, password: password };
+    let myquery = { username: username };
     db_connect.collection("Users").findOne(myquery, function (err, result) {
         if (err) throw err;
-        if (result) { res.json(result) } else { res.send('Incorrect username/password entered.') }
+        
+        if (result) { 
+            let check = bcrypt.compareSync(password, result.password);
+            if (check) { 
+                
+                const token = jwt.sign({ userId: result._id }, process.env.JWT_KEY, {
+                    expiresIn: "1h",
+                });
+                result.token = token;
+                res.json(result);
+
+            } else { 
+                res.send('Incorrect password entered');
+            }
+        } else { 
+            res.send('Incorrect username entered');
+        }
     });
 });
 
 router.post('/register', async (req, res) => {
-    
+
+    let hash = await bcrypt.hash(req.body.password, 10);
+
     const newUser = new User ({
         username: req.body.username,
-        password: req.body.password,
+        password: hash,
         name: req.body.name,
         phone: req.body.phone,
-        email: req.body.email
+        email: req.body.email,
+        token: -1
     });
 
     let db_connect = mongoUtil.getDb("AppTest");
@@ -97,9 +117,9 @@ router.post('/sendVerifyEmail', async (req, res) => {
     const userExist = await db_connect.collection("Users").findOne(userQuery);
 
     if (!userExist) {
-        res.send('An error occured when creating your account. Please try again.');
+        res.send({"message" : 'An error occured when creating your account. Please try again.', "userId" : "-1"});
     } else if (userExist.emailConfirm == 1) {
-        res.send('This account has already been verified.');
+        res.send({"message" : 'This account has already been verified.', "userId" : "-1"});
     } else {
         
         const pin = Math.floor(100000 + Math.random() * 900000);
@@ -125,7 +145,74 @@ router.post('/sendVerifyEmail', async (req, res) => {
             {_id: userExist._id}, 
             {$set: {verifyPIN: pin}}
         );
+        res.send({"message" : 'Email successfully sent!', "userId" : userExist._id});
+    }
+});
+
+router.post('/sendResetEmail', async (req, res) => {
+        
+    email = req.body.email;
+
+    let db_connect = mongoUtil.getDb("AppTest");
+    let emailQuery = { email: email };
+    const emailExist = await db_connect.collection("Users").findOne(emailQuery);
+
+    if (!emailExist) {
+        res.send('This email account does not have an account associated with it.');
+    } else {
+        
+        const pin = Math.floor(100000 + Math.random() * 900000);
+
+        const msg = {
+            to: emailExist.email,
+            from: 'liveboltsmartlock@gmail.com',
+            substitutionWrappers: ['{{', '}}'],
+            dynamicTemplateData: {
+                name: `${emailExist.name}`,
+                code: `${pin}`
+            },
+            templateId: 'd-e88b7d8285ab469398b9d4119c006fdc',
+        }
+    
+        sgMail.send(msg).then(() => {
+            console.log('Email successfully sent!');
+        }).catch((error) => {
+            res.send(error);
+        });
+
+        await db_connect.collection("Users").updateOne(
+            {_id: emailExist._id}, 
+            {$set: {verifyPIN: pin}}
+        );
         res.send('Email successfully sent!');
+    }
+});
+
+router.post('/accountVerify', async (req, res) => {
+        
+    pin = req.body.pin;
+    userId = req.body.id;
+
+    let db_connect = mongoUtil.getDb("AppTest");
+    let userQuery = { _id: new ObjectID(userId) };
+    const userExist = await db_connect.collection("Users").findOne(userQuery);
+
+    if (!userExist) {
+        res.send({"message" : 'An error has occured during the verification process. Please try again.'});
+    } else {
+        
+        if (userExist.verifyPIN == pin) {
+            await db_connect.collection("Users").updateOne(
+                { _id: userExist._id }, 
+                {
+                    $set: { emailConfirm: 1 },
+                    $unset: {verifyPIN: ''}
+                }
+            );
+            res.send({"message" : 'User has been successfully authenticated!'});
+        } else {
+            res.send({"message" : 'The PIN you have entered is incorrect. Please try again.'});
+        }
     }
 });
 
