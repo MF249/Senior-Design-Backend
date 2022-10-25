85a18ca0195e20a5bc17a97764b9564cafc3cbab
MF249 <mfaizel@knights.ucf.edu>
2022-10-25 17:39:43 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -41,7 +41,13 @@ router.post('/login', async (req, res) => {
 
                 db_connect.collection("Users").updateOne(myquery, {$set: {"token" : token}}, function(err, confirm) {
                     if (err) throw err;
-                    if (confirm.acknowledged) { res.send({ 'token' : token }) }
+                    if (confirm.acknowledged) { 
+                        if (result.emailConfirm == -1) {
+                            res.send({ 'token' : token, 'verify' : -1, 'id' : result._id});
+                        } else {
+                            res.send({ 'token' : token, 'verify' : 1 });
+                        }
+                    }
                     else { res.send({ 'message' : 'An error occured during login. Please try again.' }) }
                 });
 
