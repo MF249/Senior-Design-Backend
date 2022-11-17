89d4be8e88298e7538d060c35f964426a35bfa9a
MF249 <mfaizel@knights.ucf.edu>
2022-11-17 14:59:57 -0500

    getting connection

--- routes/mqttCom.js
+++ routes/mqttCom.js
@@ -6,40 +6,28 @@ const mqtt = require('mqtt');
 router.use(cors());
 require('dotenv').config();
 
+const connectUrl = 'mqtt://192.168.1.180';
+
 router.get('/getCon', async (req, res) => {
-    var client = mqtt.connect('mqtt://broker.mqtt-dashboard.com');
-    
-    client.on('connect', function() {
-        console.log('connected');
-        
-        client.subscribe('outTopic', function(err) {
-            if(!err) {
-                console.log('subscribed');
-                client.publish('inTopic', 'Hello from NodeJS');
-            }
-        });
-    });
-
-    res.send('Error');
-});
-
-router.get('/testCon', async (req, res) => {
-    const client  = mqtt.connect('mqtt://test.mosquitto.org');
-    console.log('connected');
-
+    var client = mqtt.connect();
     client.on('connect', function () {
-        client.subscribe('presence', function (err) {
-            if (!err) {
-                console.log('subscribed'); 
-                client.publish('presence', 'Hello mqtt');
-                console.log('published');
-            }
-        });
+        client.subscribe('first/test');
+        client.publish('first/test','Hello');
     });
+
 
     client.on('message', function (topic, message) {
         console.log(message.toString());
         client.end();
+    });
+});
+
+router.get('/testCon', async (req, res) => {
+    const client  = mqtt.connect(connectUrl);
+    client.on('connect', function (err) {
+        console.log('connnected');
+        client.subscribe('myfirst/test');
+        client.publish('myfirst/test', '1');
     });
 });
 
