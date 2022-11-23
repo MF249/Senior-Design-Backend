fa28f902efbe6d0be3ef96e01e65bb4b03a115b9
Joel Shaw <joelshaw@knights.ucf.edu>
2022-11-23 00:46:28 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -19,11 +19,48 @@ router.use(cors());
 require('dotenv').config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
 
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+async function loadSavedCredentialsIfExist() {
+    try {
+      const content = await fs.readFile(TOKEN_PATH);
+      const credentials = JSON.parse(content);
+      return google.auth.fromJSON(credentials);
+    } catch (err) {
+      return null;
+    }
+}
+  
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+      type: 'authorized_user',
+      client_id: key.client_id,
+      client_secret: key.client_secret,
+      refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+}
+  
+async function authorize() {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+      return client;
+    }
+    client = await authenticate({
+      scopes: SCOPES,
+      keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+      await saveCredentials(client);
+    }
+    return client;
+}
 
 router.get('/getAll', async (req, res) => {
         
@@ -437,46 +474,8 @@ router.post('/textUser', async (req, res) => {
     }).then(res.send("SMS sent to " + pnumber));
 });
 
-router.get('/getCamFeed', async (req, response) => {
-    async function loadSavedCredentialsIfExist() {
-        try {
-          const content = await fs.readFile(TOKEN_PATH);
-          const credentials = JSON.parse(content);
-          return google.auth.fromJSON(credentials);
-        } catch (err) {
-          return null;
-        }
-      }
-      
-      async function saveCredentials(client) {
-        const content = await fs.readFile(CREDENTIALS_PATH);
-        const keys = JSON.parse(content);
-        const key = keys.installed || keys.web;
-        const payload = JSON.stringify({
-          type: 'authorized_user',
-          client_id: key.client_id,
-          client_secret: key.client_secret,
-          refresh_token: client.credentials.refresh_token,
-        });
-        await fs.writeFile(TOKEN_PATH, payload);
-      }
-      
-      async function authorize() {
-        let client = await loadSavedCredentialsIfExist();
-        if (client) {
-          return client;
-        }
-        client = await authenticate({
-          scopes: SCOPES,
-          keyfilePath: CREDENTIALS_PATH,
-        });
-        if (client.credentials) {
-          await saveCredentials(client);
-        }
-        return client;
-      }
-      
-      async function listFiles(authClient) {
+router.get('/getCamFeed', async (req, response) => {      
+    async function listFiles(authClient) {
         const drive = google.drive({version: 'v3', auth: authClient});
         const res = await drive.files.list({
           fields: 'nextPageToken, files(webViewLink)',
@@ -489,7 +488,7 @@ router.get('/getCamFeed', async (req, response) => {
         }
       
         console.log(res.data);
-      }
+    }
       
       authorize().then(listFiles).catch(console.error);
 });
