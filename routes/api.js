43ac1a156ef91ef7f1302b18ddb282dea53dfc65
Joel Shaw <joelshaw@knights.ucf.edu>
2022-11-22 22:49:13 -0500

    c

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,9 @@ require('dotenv').config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
 router.get('/getAll', async (req, res) => {
         
     let db_connect = mongoUtil.getDb("AppTest");
@@ -433,67 +436,60 @@ router.post('/textUser', async (req, res) => {
 });
 
 router.get('/getCamFeed', async (req, response) => {
-    const SCOPES = ['https://www.googleapis.com/auth/drive'];
-    const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-    const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
-
     async function loadSavedCredentialsIfExist() {
         try {
-            const content = await fs.readFile(TOKEN_PATH);
-            const credentials = JSON.parse(content);
-            return google.auth.fromJSON(credentials);
+          const content = await fs.readFile(TOKEN_PATH);
+          const credentials = JSON.parse(content);
+          return google.auth.fromJSON(credentials);
         } catch (err) {
-            return null;
+          return null;
         }
-    }
-
-    async function saveCredentials(client) {
+      }
+      
+      async function saveCredentials(client) {
         const content = await fs.readFile(CREDENTIALS_PATH);
         const keys = JSON.parse(content);
         const key = keys.installed || keys.web;
         const payload = JSON.stringify({
-            type: 'authorized_user',
-            client_id: key.client_id,
-            client_secret: key.client_secret,
-            refresh_token: client.credentials.refresh_token,
+          type: 'authorized_user',
+          client_id: key.client_id,
+          client_secret: key.client_secret,
+          refresh_token: client.credentials.refresh_token,
         });
         await fs.writeFile(TOKEN_PATH, payload);
-    }
-
-    async function authorize() {
+      }
+      
+      async function authorize() {
         let client = await loadSavedCredentialsIfExist();
         if (client) {
-            return client;
+          return client;
         }
         client = await authenticate({
-            scopes: SCOPES,
-            keyfilePath: CREDENTIALS_PATH,
+          scopes: SCOPES,
+          keyfilePath: CREDENTIALS_PATH,
         });
         if (client.credentials) {
-            await saveCredentials(client);
+          await saveCredentials(client);
         }
         return client;
-    }
-
-    async function listFiles(authClient) {
+      }
+      
+      async function listFiles(authClient) {
         const drive = google.drive({version: 'v3', auth: authClient});
         const res = await drive.files.list({
-            fields: 'nextPageToken, files(webViewLink)',
+          fields: 'nextPageToken, files(webViewLink)',
         });
-
+      
         const files = res.data.files;
         if (files.length === 0) {
-            console.log('No capture images found.');
-            return;
+          console.log('No files found.');
+          return;
         }
-
+      
         console.log(res.data);
-
-        response.send();
-
-    }
-
-    authorize().then(listFiles).catch(console.error);
+      }
+      
+      authorize().then(listFiles).catch(console.error);
 });
 
 module.exports = router;
